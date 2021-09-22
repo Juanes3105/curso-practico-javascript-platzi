63ba37998af09e25294f2d7f386d6d5137c955f6
Juanes <74432807+Juanes3105@users.noreply.github.com>
2021-09-21 21:41:39 -0500

    Termine todos los talleres
    Y el Curso Practico de Javascript

---
+++ salarios.js
@@ -0,0 +1,66 @@
+const colombia = []
+
+colombia.push({
+    name: "Danny",
+    salary: 7472,
+});
+
+colombia.push({
+    name: "Cristina",
+    salary: 200,
+});
+
+colombia.push({
+    name: "Yeison",
+    salary: 6824,
+});
+
+colombia.push({
+    name: "Guadalupe",
+    salary: 5000,
+});
+
+colombia.push({
+    name: "Juan",
+    salary: 100000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan1",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan2",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan3",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan4",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan5",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan6",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoJuan7",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "EmpleadoDanny",
+    salary: 5000,
+});
