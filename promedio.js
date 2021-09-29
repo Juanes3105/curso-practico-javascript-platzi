5ab9ead412fd4a51909729b0d7771e5144808025
Juanes <74432807+Juanes3105@users.noreply.github.com>
2021-09-29 09:04:49 -0500

    Hice cambios en las paginas de los talleres

--- promedio.js
+++ promedio.js
@@ -1,17 +1,15 @@
 
-function calcularMediaAritmetica(lista) {
-  // let sumaLista = 0;
-  // for (let i = 0; i < lista.length; i++) {
-  //   sumaLista = sumaLista + lista[i];
-  // }
+function calcularPromedio() {
+  const input = document.getElementById("lista");
+  const value = input.value;
 
-  const sumaLista = lista.reduce(
+  const sumaLista = value.reduce(
     function (valorAcumulado = 0, nuevoElemento) {
       return valorAcumulado + nuevoElemento;
     }
   );
 
-  const promedioLista = sumaLista / lista.length;
+  const promedioLista = sumaLista / value.length;
 
-  return promedioLista;
+  document.write(`El promedio de esta lista es: ${promedioLista}`)
 }
