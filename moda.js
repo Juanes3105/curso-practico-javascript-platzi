97c5df914600cbd3cca897d923513bffaafaebb2
Juanes <74432807+Juanes3105@users.noreply.github.com>
2021-11-19 11:23:33 -0500

    Add files via upload

--- moda.js
+++ moda.js
@@ -1,18 +1,17 @@
-const lista1 = [
-  1,
-  2,
-  3,
-  1,
-  2,
-  3,
-  4,
-  2,
-  2,
-  2,
-  1,
-];
-
+const lista1 = [];
 const lista1Count = {};
+
+function meterValorLista() {
+  const input = document.getElementById("inputLista")
+  const value = input.value
+  lista1.push(Number(value))
+  const lista = document.getElementById("lista")
+  lista.innerText = lista1
+}
+
+function calcularModa() {
+  
+
 
 lista1.map(
   function (elemento) {
@@ -31,4 +30,6 @@ const lista1Array = Object.entries(lista1Count).sort(
   }
 );
 
-const lista1
+
+
+}
