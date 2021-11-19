05bc9c32a3c67d4278a09fa014dfc42babf9a8aa
Juanes <74432807+Juanes3105@users.noreply.github.com>
2021-11-19 14:35:55 -0600

    Termine el projecto

--- salarios.js
+++ salarios.js
@@ -1,66 +1,10 @@
+function meterValorLista() {
+    const input = document.getElementById("inputLista")
+    const value = input.value
+    colombia.push(Number(value))
+    const lista = document.getElementById("lista")
+    lista.innerText = colombial
+  }
+
+
 const colombia = []
-
-colombia.push({
-    name: "Danny",
-    salary: 7472,
-});
-
-colombia.push({
-    name: "Cristina",
-    salary: 200,
-});
-
-colombia.push({
-    name: "Yeison",
-    salary: 6824,
-});
-
-colombia.push({
-    name: "Guadalupe",
-    salary: 5000,
-});
-
-colombia.push({
-    name: "Juan",
-    salary: 100000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan1",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan2",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan3",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan4",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan5",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan6",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoJuan7",
-    salary: 1000,
-});
-
-colombia.push({
-    name: "EmpleadoDanny",
-    salary: 5000,
-});
